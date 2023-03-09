2f264f4074f331c56ad12de7681f6de3c4a025f2
unknown <bhotnikhil25@gmail.com>
2023-03-09 20:50:20 +0530

    [Nikhil]-Ability To Create Simple Linked List

---
+++ LinkListExample/LinkListExample/LinkedListOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListExample
+{
+    public class LinkedListOperation
+    {
+        internal Node head;
+        internal void add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null) 
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} INSERTED INTO LINKED LIST",node.data);
+        }
+    }
+}
---
+++ LinkListExample/LinkListExample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkListExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * UC-01-Lets create a simple Linked List of 56, 30 and 70
+            */
+            LinkedListOperation linkedListOperation =new LinkedListOperation(); //Creation Of Object Of LinkedListOperation Class
+            linkedListOperation.add(56); //with using object of class we call the method add and pass data 56
+            linkedListOperation.add(30); //with using object of class we call the method add and pass data 30
+            linkedListOperation.add(70); //with using object of class we call the method add and pass data 70
+        }
+    }
+}
