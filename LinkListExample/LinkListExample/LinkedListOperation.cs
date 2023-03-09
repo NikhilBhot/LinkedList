1402e215a8637f9e63fb9e853e074a7cda25b6c8
unknown <bhotnikhil25@gmail.com>
2023-03-09 21:21:51 +0530

    [Nikhil]-Ability To Insert Data At Particular Postion

--- LinkListExample/LinkListExample/LinkedListOperation.cs
+++ LinkListExample/LinkListExample/LinkedListOperation.cs
@@ -28,5 +28,54 @@ namespace LinkListExample
             }
             Console.WriteLine("{0} INSERTED INTO LINKED LIST",node.data);
         }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        /*
+         * UC-04-Ability to insert 30 between 56 and 70
+         * i.e Ability To Insert Data At Particular Postion
+         */
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0 && temp != null)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = temp.next;
+                        temp.next = node;
+                        break;
+                    }
+
+                    temp = temp.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
     }
 }
--- LinkListExample/LinkListExample/Program.cs
+++ LinkListExample/LinkListExample/Program.cs
@@ -13,6 +13,14 @@
             linkedListOperation.add(30); //with using object of class we call the method add and pass data 56
             linkedListOperation.add(70); //with using object of class we call the method add and pass data 30
             linkedListOperation.add(56); //with using object of class we call the method add and pass data 70
+            linkedListOperation.Display();
+            /*
+              * UC-04-Ability to insert 30 between 56 and 70
+              * i.e Ability To Insert Data At Particular Postion
+            */
+            linkedListOperation.InsertAtParticularPosition(2, 81);
+            Console.WriteLine();
+            linkedListOperation.Display();
         }
     }
 }
