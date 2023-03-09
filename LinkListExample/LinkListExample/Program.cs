0be65b2287b9a51ae2a21717f80a71eadc5fc9a8
unknown <bhotnikhil25@gmail.com>
2023-03-09 21:09:39 +0530

    [Nikhil]-Ability to create Linked List by adding 30 and 70 to 56

--- LinkListExample/LinkListExample/Program.cs
+++ LinkListExample/LinkListExample/Program.cs
@@ -7,10 +7,11 @@
             /*
              * UC-01-Lets create a simple Linked List of 56, 30 and 70
              * UC-02-Ability to create Linked List by adding 30 and 56 to 70
+             * UC-03Ability to create Linked List by appending 30 and 70 to 56
             */
             LinkedListOperation linkedListOperation =new LinkedListOperation(); //Creation Of Object Of LinkedListOperation Class
-            linkedListOperation.add(70); //with using object of class we call the method add and pass data 56
-            linkedListOperation.add(30); //with using object of class we call the method add and pass data 30
+            linkedListOperation.add(30); //with using object of class we call the method add and pass data 56
+            linkedListOperation.add(70); //with using object of class we call the method add and pass data 30
             linkedListOperation.add(56); //with using object of class we call the method add and pass data 70
         }
     }
